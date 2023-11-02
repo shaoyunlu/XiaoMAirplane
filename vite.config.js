0f7bdc473b85e478dfc3204abcb73b331b62e958
shaoyunlu <shaoyunlu@126.com>
2023-11-02 18:02:19 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
+import html from '@rollup/plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    cssCodeSplit: false,
     minify: false,
     watch: true,
-    outDir: 'build',
+    outDir: 'dist',
     rollupOptions: {
       input: 'src/main.js',
       output: {
-        entryFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
       }
     }
   },
@@ -20,6 +23,31 @@ export default defineConfig({
             legacy({
               targets:['defaults','not IE 11']
             }),
-            vue()
+            vue(),
+            html({
+              fileName: 'index.html', // 生成的 HTML 文件名
+              title: 'My App', // HTML 文件的标题
+              template : ()=>{
+                return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="/assets/style.css">
+      <title>Document</title>
+  </head>
+  <body>
+      <div id="xm-airplane"></div>
+
+      <script src="/assets/polyfills-legacy.js"></script>
+      <script src="/system.js"></script>
+      <script src="/assets/main-legacy.js"></script>
+  </body>
+</html>
+                        `
+              }
+            })
           ]
 })
